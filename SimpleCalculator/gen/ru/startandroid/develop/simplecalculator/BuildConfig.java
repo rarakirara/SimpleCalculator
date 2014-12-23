5f7fd16e521542f2503400a89be59a857003f0f2
rarakirara <anton26lk@mail.ru>
2014-12-23 14:43:39 +0700

    it is simple calculator

---
+++ SimpleCalculator/gen/ru/startandroid/develop/simplecalculator/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.startandroid.develop.simplecalculator;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
